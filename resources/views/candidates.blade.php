9d99cf63308715cb290b5cb05466b93740e9316f
Yusuf Olatunji <yoyforever@gmail.com>
2024-09-04 18:06:17 +0100

    removing dcss content

--- resources/views/candidates.blade.php
+++ resources/views/candidates.blade.php
@@ -9,7 +9,7 @@
             <div class="row y-gap-50 items-center">
                 <div class="row">
                     <div class="col-12">
-                        <h2 class="text-center text-24 text-red-1">RELEASE OF 2024/2025 COMMAND SECONDARY SCHOOLS COMMON ENTRANCE EXAMINATION RESULT</h2>
+                        <h2 class="text-center text-24 text-red-1"></h2>
                         <div class="row mt-4">
                             <div class="col-lg-6 col-12">
                                 <h6 class="text-center text-17 text-red-1">COMMAND DAY SECONDARY SCHOOL LISTS</h6>
@@ -70,7 +70,7 @@
 
 @section('top_news')
     <div class="d-flex items-center text-white py-10 border-bottom-light" style="background-color: #00004b;">
-        <marquee behavior="alternate" scrollamount="5" direction="left" onmouseover="this.stop();" onmouseout="this.start();" style="padding: 5px 1px"> <a href="#" style="color: white; font-weight: bold; text-transform: uppercase">RELEASE OF 2024/2025 COMMAND SECONDARY SCHOOLS COMMON ENTRANCE EXAMINATION RESULT<span style="background-color: red; padding: 5px 10px; margin-left: 5px; margin-right: 5px">
+        <marquee behavior="alternate" scrollamount="5" direction="left" onmouseover="this.stop();" onmouseout="this.start();" style="padding: 5px 1px"> <a href="#" style="color: white; font-weight: bold; text-transform: uppercase"><span style="background-color: red; padding: 5px 10px; margin-left: 5px; margin-right: 5px">
  </span><span style="color: BLACK"></span></a></marquee>
     </div>
 @endsection
